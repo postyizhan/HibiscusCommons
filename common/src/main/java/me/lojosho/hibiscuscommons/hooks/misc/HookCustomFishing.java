a6e1a1d9c1a5774dac9591f6b5a4c27e66f62f0f
LoJoSho <LoJoShogaming@gmail.com>
2023-12-29 11:34:20 -0600

    fix: CustomFishing integration

--- common/src/main/java/me/lojosho/hibiscuscommons/hooks/misc/HookCustomFishing.java
+++ common/src/main/java/me/lojosho/hibiscuscommons/hooks/misc/HookCustomFishing.java
@@ -2,13 +2,10 @@ package me.lojosho.hibiscuscommons.hooks.misc;
 
 import me.lojosho.hibiscuscommons.api.events.HibiscusPluginFishEvent;
 import me.lojosho.hibiscuscommons.hooks.Hook;
-import net.momirealms.customfishing.api.event.FishingResultEvent;
-import net.momirealms.customfishing.api.mechanic.loot.LootType;
+import net.momirealms.customfishing.api.event.FishingLootSpawnEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.inventory.ItemStack;
 
 public class HookCustomFishing extends Hook {
 
@@ -17,10 +14,8 @@ public class HookCustomFishing extends Hook {
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onPlayerFish(FishingResultEvent event) {
-        // TODO: Finish this once I get a firmer response on how this api is suppose to work
-        if (!event.getLoot().getType().equals(LootType.ITEM)) return;
-        HibiscusPluginFishEvent newEvent = new HibiscusPluginFishEvent(this, event.getPlayer(), new ItemStack(Material.ICE));
+    public void onPlayerFish(FishingLootSpawnEvent event) {
+        HibiscusPluginFishEvent newEvent = new HibiscusPluginFishEvent(this, event.getPlayer(), event.getItemStack());
         Bukkit.getPluginManager().callEvent(newEvent);
     }
 }
