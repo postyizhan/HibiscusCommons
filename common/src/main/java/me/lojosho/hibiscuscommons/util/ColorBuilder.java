8efe084d3a86fee104adae65404567080680893d
LoJoSho <LoJoShogaming@gmail.com>
2024-05-19 19:43:13 -0500

    feat: update color builder to support Firework Effect Meta

--- common/src/main/java/me/lojosho/hibiscuscommons/util/ColorBuilder.java
+++ common/src/main/java/me/lojosho/hibiscuscommons/util/ColorBuilder.java
@@ -1,8 +1,10 @@
 package me.lojosho.hibiscuscommons.util;
 
 import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.FireworkEffectMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.inventory.meta.PotionMeta;
@@ -17,8 +19,10 @@ public class ColorBuilder {
     public static boolean canBeColored(final @NotNull ItemStack itemStack) {
         final ItemMeta itemMeta = itemStack.getItemMeta();
 
-        return (itemMeta instanceof LeatherArmorMeta ||
-                itemMeta instanceof PotionMeta);
+        return (itemMeta instanceof LeatherArmorMeta
+                || itemMeta instanceof PotionMeta
+                || itemMeta instanceof FireworkEffectMeta
+        );
     }
 
     /**
@@ -32,6 +36,9 @@ public class ColorBuilder {
         }
         if (itemMeta instanceof final LeatherArmorMeta meta) {
             meta.setColor(color);
+        }
+        if (itemMeta instanceof final FireworkEffectMeta meta) {
+            meta.setEffect(FireworkEffect.builder().withColor(color).build());
         }
         return itemMeta;
     }
