4697fc5b3127970db628bd995e98868d328915d3
LoJoSho <LoJoShogaming@gmail.com>
2024-12-29 16:49:55 -0600

    chore: add downloadPlugins to runServer task

--- build.gradle.kts
+++ build.gradle.kts
@@ -139,6 +139,12 @@ tasks {
         dependsOn(shadowJar)
         dependsOn(jar)
         minecraftVersion("1.21.1")
+
+        downloadPlugins {
+            hangar("PlaceholderAPI", "2.11.6")
+            url("https://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/build/libs/ProtocolLib.jar")
+            url("https://download.luckperms.net/1567/bukkit/loader/LuckPerms-Bukkit-5.4.150.jar")
+        }
     }
 
     javadoc {
