cd27258bf20cf944cf0c740a955552bb86b367ed
siansxint <joshua1504.jr@gmail.com>
2025-01-07 21:34:23 -0400

    feat: add AllHooksActiveEvent so external plugins actually know when to load their stuff

---
+++ common/src/main/java/me/lojosho/hibiscuscommons/api/events/AllHooksActiveEvent.java
@@ -0,0 +1,19 @@
+package me.lojosho.hibiscuscommons.api.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class AllHooksActiveEvent extends Event {
+
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+}
