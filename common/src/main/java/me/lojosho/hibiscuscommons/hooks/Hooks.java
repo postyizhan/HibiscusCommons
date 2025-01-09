d1ce3d2737dde7ebd1bde48100171d9c542a9767
LoJoSho <lojoshogaming@gmail.com>
2025-01-09 15:53:54 -0600

    fix: readd checkHookLoadingStatus at end of setup method (fixes not running event when no late loads are there)

--- common/src/main/java/me/lojosho/hibiscuscommons/hooks/Hooks.java
+++ common/src/main/java/me/lojosho/hibiscuscommons/hooks/Hooks.java
@@ -78,6 +78,8 @@ public class Hooks {
                 HibiscusCommonsPlugin.getInstance().getLogger().info("Successfully hooked into " + hook.getId());
             }
         }
+
+        checkHookLoadingStatus();
     }
 
     /**
