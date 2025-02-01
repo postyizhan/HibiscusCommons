75ea857f6fb415f58af557eac23d0a314453ed95
LoJoSho <LoJoShogaming@gmail.com>
2025-02-01 09:54:48 -0600

    feat: add custom fishing items

--- common/src/main/java/me/lojosho/hibiscuscommons/hooks/misc/HookCustomFishing.java
+++ common/src/main/java/me/lojosho/hibiscuscommons/hooks/misc/HookCustomFishing.java
@@ -2,16 +2,26 @@ package me.lojosho.hibiscuscommons.hooks.misc;
 
 import me.lojosho.hibiscuscommons.api.events.HibiscusPluginFishEvent;
 import me.lojosho.hibiscuscommons.hooks.Hook;
+import me.lojosho.hibiscuscommons.hooks.HookFlag;
+import net.momirealms.customfishing.api.BukkitCustomFishingPlugin;
 import net.momirealms.customfishing.api.event.FishingLootSpawnEvent;
+import net.momirealms.customfishing.api.mechanic.context.Context;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 public class HookCustomFishing extends Hook {
 
     public HookCustomFishing() {
-        super("CustomFishing");
+        super("CustomFishing", HookFlag.ITEM_SUPPORT);
+    }
+
+    @Override
+    public ItemStack getItem(@NotNull String itemId) {
+        return BukkitCustomFishingPlugin.getInstance().getItemManager().buildAny(Context.player(null), itemId);
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
