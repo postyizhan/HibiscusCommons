49fe13d8bd07d31b5d27d49794cb500084ca768a
LoJoSho <LoJoShogaming@gmail.com>
2025-02-25 19:19:20 -0600

    chore: update adventure dependency to 4.19.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -107,9 +107,9 @@ allprojects {
         testAnnotationProcessor("org.projectlombok:lombok:1.18.36")
 
         // Spigot Auto Loader Libraries
-        compileOnly("net.kyori:adventure-api:4.18.0")
-        compileOnly("net.kyori:adventure-text-minimessage:4.18.0")
-        compileOnly("net.kyori:adventure-text-serializer-gson:4.18.0")
+        compileOnly("net.kyori:adventure-api:4.19.0")
+        compileOnly("net.kyori:adventure-text-minimessage:4.19.0")
+        compileOnly("net.kyori:adventure-text-serializer-gson:4.19.0")
         compileOnly("net.kyori:adventure-platform-bukkit:4.3.4")
         compileOnly("org.apache.commons:commons-lang3:3.17.0")
 
@@ -216,9 +216,9 @@ bukkit {
     )
 
     libraries = listOf(
-        "net.kyori:adventure-api:4.18.0",
-        "net.kyori:adventure-text-minimessage:4.18.0",
-        "net.kyori:adventure-text-serializer-gson:4.18.0",
+        "net.kyori:adventure-api:4.19.0",
+        "net.kyori:adventure-text-minimessage:4.19.0",
+        "net.kyori:adventure-text-serializer-gson:4.19.0",
         "net.kyori:adventure-platform-bukkit:4.3.4",
         "org.apache.commons:commons-lang3:3.17.0"
         //"org.spongepowered:configurate-yaml:4.2.0-SNAPSHOT" // Readd when 4.2.0 releases
