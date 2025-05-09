7ef40c1f0f0e5a5a69e581d8c9112e8344b33348
Boy0000 <sivertpaulsen2@gmail.com>
2025-05-09 15:12:08 +0200

    feat: add nms-methods for getting color of item

--- common/src/main/java/me/lojosho/hibiscuscommons/nms/NMSUtils.java
+++ common/src/main/java/me/lojosho/hibiscuscommons/nms/NMSUtils.java
@@ -1,12 +1,22 @@
 package me.lojosho.hibiscuscommons.nms;
 
+import org.bukkit.Color;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
 
 public interface NMSUtils {
 
     int getNextEntityId();
 
     Entity getEntity(int entityId);
+
+    @Nullable
+    Color getColor(ItemStack itemStack);
+
+    ItemStack setColor(@NotNull ItemStack itemStack, Color color);
 
 }
--- v1_20_R3/src/main/java/me/lojosho/hibiscuscommons/nms/v1_20_R3/NMSUtils.java
+++ v1_20_R3/src/main/java/me/lojosho/hibiscuscommons/nms/v1_20_R3/NMSUtils.java
@@ -2,7 +2,13 @@ package me.lojosho.hibiscuscommons.nms.v1_20_R3;
 
 import net.minecraft.server.level.ServerLevel;
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
 import org.bukkit.craftbukkit.v1_20_R3.CraftServer;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.*;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class NMSUtils extends NMSCommon implements me.lojosho.hibiscuscommons.nms.NMSUtils {
 
@@ -16,6 +22,51 @@ public class NMSUtils extends NMSCommon implements me.lojosho.hibiscuscommons.nm
         net.minecraft.world.entity.Entity entity = getNMSEntity(entityId);
         if (entity == null) return null;
         return entity.getBukkitEntity();
+    }
+
+    @Override
+    public @Nullable Color getColor(ItemStack itemStack) {
+        if (itemStack == null) return null;
+        ItemMeta meta = itemStack.getItemMeta();
+        if (meta == null) return null;
+
+        Color color = null;
+        switch (meta) {
+            case LeatherArmorMeta leatherMeta -> color = leatherMeta.getColor();
+            case PotionMeta potionMeta -> color = potionMeta.getColor();
+            case MapMeta mapMeta -> color = mapMeta.getColor();
+            case FireworkEffectMeta fireworkEffectMeta -> {
+                FireworkEffect effect = fireworkEffectMeta.getEffect();
+                if (effect != null) {
+                    color = effect.getColors().stream().findFirst().isPresent() ? effect.getColors().stream().findFirst().get() : null;
+                }
+            }
+            default -> {}
+        }
+
+        return color;
+    }
+
+    @Override
+    public ItemStack setColor(@NotNull ItemStack itemStack, Color color) {
+        ItemMeta meta = itemStack.getItemMeta();
+        switch (meta) {
+            case LeatherArmorMeta leatherMeta -> leatherMeta.setColor(color);
+            case PotionMeta potionMeta -> potionMeta.setColor(color);
+            case MapMeta mapMeta -> mapMeta.setColor(color);
+            case FireworkEffectMeta fireworkMeta -> fireworkMeta.setEffect(
+                    FireworkEffect.builder()
+                            .with(FireworkEffect.Type.BALL)
+                            .withColor(color)
+                            .trail(false)
+                            .flicker(false)
+                            .build()
+            );
+            case null, default -> {}
+        }
+
+        itemStack.setItemMeta(meta);
+        return itemStack;
     }
 
     private net.minecraft.world.entity.Entity getNMSEntity(int entityId) {
